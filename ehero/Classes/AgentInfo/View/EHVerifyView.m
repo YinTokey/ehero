7578354c1f570e9f2e9bea298c824b5343d8efc2
YinTokey <YinjChen@163.com>
2016-08-05 18:51:48 +0800

    实验和网页类似的验证码倒计时效果

--- ehero/Classes/AgentInfo/View/EHVerifyView.m
+++ ehero/Classes/AgentInfo/View/EHVerifyView.m
@@ -7,12 +7,15 @@
 //
 
 #import "EHVerifyView.h"
+#import "MZTimerLabel.h"
+
 
 @interface EHVerifyView()
 
 - (IBAction)callBtnClick:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *code;
 @property (weak, nonatomic) IBOutlet UITextField *myPhoneNumber;
+@property (nonatomic,strong) UIButton *sendCodeBtn;
 
 
 @end
@@ -33,7 +36,10 @@
     sendCodeBtn.titleLabel.textColor = [UIColor whiteColor];
     [sendCodeBtn setBackgroundImage:[UIImage imageNamed:@"sendCodeBtn"] forState:UIControlStateNormal];
     [sendCodeBtn addTarget:self action:@selector(sendClick) forControlEvents:UIControlEventTouchUpInside];
-    self.myPhoneNumber.rightView = sendCodeBtn;
+    self.sendCodeBtn =  sendCodeBtn;
+    
+    
+    self.myPhoneNumber.rightView = _sendCodeBtn;
     self.myPhoneNumber.rightViewMode = UITextFieldViewModeAlways;
     //添加边框
     self.code.layer.borderColor = [[UIColor lightGrayColor]CGColor];
@@ -43,7 +49,15 @@
 
 - (void)sendClick{
     NSLog(@"发验证码测试");
-
+    MZTimerLabel *timer = [[MZTimerLabel alloc] initWithLabel:self.sendCodeBtn.titleLabel andTimerType:MZTimerLabelTypeTimer];
+    timer.timeFormat = @"ss 秒";
+    [timer setCountDownTime:60];
+    
+    [self.sendCodeBtn setFrame:CGRectMake(0, 0, self.myPhoneNumber.bounds.size.width / 6, self.myPhoneNumber.bounds.size.height)];
+    
+    [timer start];
+    
+    
 }
 
 - (IBAction)callBtnClick:(id)sender {
