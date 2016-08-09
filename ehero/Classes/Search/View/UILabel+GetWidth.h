e54b84e778ac62ad51ca8db0aa6c70df389fe2cb
YinTokey <YinjChen@163.com>
2016-08-09 16:34:43 +0800

    UILabel添加类别，自适应宽度

---
+++ ehero/Classes/Search/View/UILabel+GetWidth.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+GetWidth.h
+//  ehero
+//
+//  Created by Mac on 16/8/9.
+//  Copyright © 2016年 ehero. All rights reserved.
+//
+
+
+
+@interface UILabel (GetWidth)
+
++ (CGFloat)getWidthWithTitle:(NSString *)title font:(UIFont *)font;
+
+@end
