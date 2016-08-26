e5a74d42217accee8a8981ab98586988b08b7ba4
YinTokey <YinjChen@163.com>
2016-08-26 18:10:32 +0800

    通过注入js代码，获取网页图片链接

--- ehero/Classes/OfficialAccount/Controller/EHOfficialAccountController.m
+++ ehero/Classes/OfficialAccount/Controller/EHOfficialAccountController.m
@@ -19,49 +19,64 @@
 
 - (void)viewDidLoad{
     [super viewDidLoad];
+    [LBProgressHUD showHUDto:self.view animated:NO];
     
-    NSString *js = @"function addImgClickEvent() { \
-    var imgs = document.getElementsByTagName('img'); \
-    for (var i = 0; i < imgs.length; ++i) { \
-    var img = imgs[i]; \
-    img.onclick = function () { \
-    window.location.href = 'hyb-image-preview:' + this.src; \
-    }; \
-    } \
-    }";
-    
-    // 根据JS字符串初始化WKUserScript对象
-    WKUserScript *script = [[WKUserScript alloc] initWithSource:js injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
-    
-    // 根据生成的WKUserScript对象，初始化WKWebViewConfiguration
-    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    [config.userContentController addUserScript:script];
-    
-    _webView = [[WKWebView alloc]initWithFrame:self.view.frame configuration:config];
+    _webView = [[WKWebView alloc]initWithFrame:self.view.frame];
     NSURL *url = [NSURL URLWithString:self.href];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    self.webView.navigationDelegate = self;
     [_webView loadRequest:request];
     [self.view addSubview:_webView];
-    
-    self.webView.navigationDelegate = self;
 
-    NSLog(@"BEGIN");
     
 }
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-    [LBProgressHUD showHUDto:self.view animated:NO];
+   
     NSLog(@"star load");
+
 }
+
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+    
+    if ([navigationAction.request.URL.scheme hasPrefix:@"hyb-image-preview"]) {
+        // 获取原始图片的完整URL
+        NSString *src = [navigationAction.request.URL.absoluteString stringByReplacingOccurrencesOfString:@"hyb-image-preview:" withString:@""];
+        if (src.length > 0) {
+  
+            NSLog(@"所点击的HTML中的img标签的图片的URL为：%@", src);
+        }
+    }
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
     [LBProgressHUD hideAllHUDsForView:self.view animated:NO];
     NSLog(@"end load");
-    
-    
-    
+    //注入js代码
+    [self.webView evaluateJavaScript:@"function addImgClickEvent() { \
+     var imgs = document.getElementsByTagName('img'); \
+     for (var i = 0; i < imgs.length; ++i) { \
+     var img = imgs[i]; \
+     img.onclick = function () { \
+     window.location.href = 'hyb-image-preview:' + this.src; \
+     }; \
+     } \
+     }" completionHandler:^(id Result , NSError * _Nullable error) {
+
+     }];
+    //执行js
+    [self.webView evaluateJavaScript:@"addImgClickEvent();" completionHandler:^(id Result, NSError * error) {
+        NSLog(@"c:%@",Result);
+        NSLog(@"err %@",error);
+    }];
     
 }
+
 
 
 
